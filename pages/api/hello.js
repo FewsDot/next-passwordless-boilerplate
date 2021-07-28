1bfe36b97da7f83a00d2db45b227a0417740885b
JustFuknDevIt <justfukndevit@gmail.com>
2021-07-28 14:14:35 +0200

    add save token in db

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,17 +1,25 @@
-import {} from "ramda";
+import { connectToDatabase } from "lib/mongodb";
 import { checkAuth, checkMethod } from "lib/requestChecker";
 import { generateRandomToken, saveTokenInDB } from "lib/token";
 
 // SIDES EFFECTS
-const authToken = generateRandomToken();
 
-export default function handler(req, res) {
+const handler = async (req, res) => {
+	//Destructure db
+	const { db } = await connectToDatabase();
+	const collectionToken = db.collection("token");
+
 	// 1 - Recoit un input, le verifie, et retourne un objet
 	const bodyResult = checkAuth(req.body.email);
 	const methodResult = checkMethod(req);
 
 	// 2 - Genere un token d'authentification
-	// 3
+	const authToken = generateRandomToken();
 
-	res.status(200).json({ response: bodyResult, method: methodResult, authToken });
-}
+	// 3 - Sauvegarde le token en base de donnée
+	const tokenResult = await saveTokenInDB(collectionToken, authToken);
+
+	res.status(200).json({ response: bodyResult, method: methodResult, token: tokenResult });
+};
+
+export default handler;
