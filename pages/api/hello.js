9a52d8a5429705e94c6094b79c545adb98aeb194
JustFuknDevIt <justfukndevit@gmail.com>
2021-07-28 11:15:15 +0200

    add checker for method and body of the request

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {} from "ramda";
+import { checkAuth, checkMethod } from "lib/requestChecker";
+import { generateRandomToken, saveTokenInDB } from "lib/token";
+
+// SIDES EFFECTS
+const authToken = generateRandomToken();
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+	// 1 - Recoit un input, le verifie, et retourne un objet
+	const bodyResult = checkAuth(req.body.email);
+	const methodResult = checkMethod(req);
+
+	// 2 - Genere un token d'authentification
+	// 3
+
+	res.status(200).json({ response: bodyResult, method: methodResult, authToken });
 }
