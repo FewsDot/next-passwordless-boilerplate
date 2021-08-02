a0399ec3b4ef6da6c74ccc52adf3d776a98fac2e
JustFuknDevIt <justfukndevit@gmail.com>
2021-08-02 14:29:07 +0200

    refacto and add data saving error

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,29 +1,33 @@
 import { connectToDatabase } from "lib/mongodb";
 import { checkAuth, checkMethod } from "lib/requestChecker";
-import { getUserInDB, saveUserInDB } from "lib/users";
-import { generateRandomToken, saveTokenInDB } from "lib/token";
+import { getUserInDB, createUser } from "lib/users";
+import { nowInTimestamp } from "lib/time";
+import { saveInDB } from "lib/database";
+import { generateRandomToken } from "lib/token";
 import { sendAuthMail } from "lib/mailer";
 
 const handler = async (req, res) => {
 	const { db } = await connectToDatabase();
-	const tokenCollection = db.collection("token");
+	const tokensCollection = db.collection("tokens");
 	const usersCollection = db.collection("users");
+	const errorsCollection = db.collection("errors");
+	const now = nowInTimestamp();
 
 	try {
 		checkMethod(req);
 		checkAuth(req.body.email);
 		const { email } = req.body;
-		const authToken = generateRandomToken();
+		const authToken = generateRandomToken(now);
 		const isUserExist = await getUserInDB(usersCollection, email);
-		!isUserExist.data && (await saveUserInDB(usersCollection, email));
-		await saveTokenInDB(tokenCollection, authToken);
-		await sendAuthMail(email, authToken.token);
+		!isUserExist.data && (await saveInDB(usersCollection, createUser(email, now)));
+		await saveInDB(tokensCollection, authToken);
+		await sendAuthMail(email, authToken);
 
 		res.status(200).json({ status: "succes", message: "Please check you mail !" });
 	} catch (error) {
 		if (error.type === "dependence") {
-			console.log("dependance error");
-			//TODO: Creer fonction qui sauvegarde l'erreur dans la bdd
+			await saveInDB(errorsCollection, error);
+			res.status(400).json({ error: "dependance error", message: "Error send to the webmaster." });
 		}
 		res.status(400).json(error);
 	}
