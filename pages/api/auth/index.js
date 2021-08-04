fe6737e2055f4b223c88b7cbcdff481dc8593b49
Perso <jilan.menzer@yahoo.fr>
2021-08-04 07:57:46 +0200

    refacto auth and error mailer

--- pages/api/auth/index.js
+++ pages/api/auth/index.js
@@ -1,5 +1,5 @@
 import { connectToDatabase } from "lib/mongodb";
-import { checkExistence, checkEmail, checkScript, checkMethod } from "lib/requestChecker";
+import { pipeAuthCheck } from "lib/requestChecker";
 import { getUser, createUser } from "lib/users";
 import { nowInTimestamp } from "lib/time";
 import { saveInDB } from "lib/database";
@@ -9,32 +9,29 @@ import { sendAuthMail } from "lib/mailer";
 const handler = async (req, res) => {
 	const { db } = await connectToDatabase(); //Init DB
 	const now = nowInTimestamp(); //Get actual Timestamp
-	const {
-		body: { email },
-		method,
-	} = req; // Request destructuration
-	//TODO: Create One Function for all checkings
+	const { email } = req.body;
+	const method = req.method;
 
 	try {
-		checkMethod(method, "POST");
-		checkScript(email);
-		checkExistence(email, "email");
-		checkEmail(email);
+		pipeAuthCheck(method, "POST", email, "email"); //Checking on Request
 		const user = await getUser(db.collection("users"), email); //Check If User
 		const newUser = !user && (await saveInDB(db.collection("users"), createUser(email, now))); //If not Create New User
-		const authToken = generateRandomToken(now, user ? user._id : newUser.insertedId); //Create Auth Token
+		const userID = user ? user._id : newUser.insertedId; //Get UserID
+		const authToken = generateRandomToken(now, userID); //Create Auth Token
 		await saveInDB(db.collection("tokens"), authToken); //Save Token In DB
 		await sendAuthMail(email, authToken); //Send Token To User
 		res.status(200).json({
+			status: "succes",
 			type: user ? "SignIn" : "SignUp",
 			message: "Please check your email",
 		});
 	} catch (error) {
-		if (error.type === "dependence") {
-			await saveInDB(db.collection("errors"), error); //Save dependence error in DB
-			res.status(400).json({ error: "dependance error", message: "Error send to the webmaster." });
-		}
-		res.status(400).json(error);
+		!error.type
+			? res.status(400).json(error) //Return Normal Error
+			: (await saveInDB(db.collection("errors"), { step: "auth", timestamp: now, error }), //Save dependence error in DB
+			  res
+					.status(400)
+					.json({ error: "dependance error", message: "Error send to the webmaster." }));
 	}
 };
 
