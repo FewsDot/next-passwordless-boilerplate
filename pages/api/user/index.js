ebb058fce8c4f778b3b47eed5043a2eed4d78f97
Perso <jilan.menzer@yahoo.fr>
2021-08-06 10:35:59 +0200

    add feature to the api

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,15 +1,24 @@
-import { saveInDB } from "lib/backend/database";
-import { nowInTimestamp } from "lib/backend/time";
+import { connectToDatabase } from "lib/backend/mongodb";
+import { ObjectId } from "mongodb";
+import { getInDB } from "lib/backend/database";
 import { pipeBearerCheck } from "lib/backend/requestChecker";
 
 const handler = async (req, res) => {
+	const { db } = await connectToDatabase(); //Init DB
+
 	try {
 		const { authorization } = req.headers; //Get JWT from Request Header
-		pipeBearerCheck(authorization, "Bearer Token"); // Check If JWT exist and is not expired.
-		//TODO: Get User info from DB with user id contenue dans son jwt decodé
+		const { userID } = pipeBearerCheck(authorization, "Bearer Token"); // Check If JWT exist and is not expired.
+		//TODO: //Get User info from DB with user id contenue dans son jwt decodé
+		const userFromDB = await getInDB(
+			db.collection("users"),
+			{ _id: ObjectId(userID) },
+			{ status: "Error", message: "User Not Exist !" }
+		);
+
 		res.status(200).json({
 			status: "Succes",
-			now: nowInTimestamp(),
+			userFromDB,
 		});
 	} catch (error) {
 		!error.type
